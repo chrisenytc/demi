301a27da5ad28022ec22e47c5b3e17fc750df1ee
Christopher EnyTC <chris.enytc@gmail.com>
2014-02-21 19:29:30 -0300

    Upload v0.3.3

--- api/controllers/routes.js
+++ api/controllers/routes.js
@@ -10,44 +10,44 @@
 
 module.exports = {
 
- /*
-  * GET /
-  */
+  /*
+   * GET /
+   */
 
- index: {
-  method: 'GET',
-  fn: function (req, res) {
-   res.jsonp(200, {
-    message: 'Request received successfully!',
-    method: req.method
-   });
+  index: {
+    method: 'GET',
+    fn: function(req, res) {
+      res.jsonp(200, {
+        message: 'Request received successfully!',
+        method: req.method
+      });
+    }
+  },
+  post: {
+    method: 'POST',
+    fn: function(req, res) {
+      res.jsonp(200, {
+        message: 'Request received successfully!',
+        method: req.method
+      });
+    }
+  },
+  put: {
+    method: 'PUT',
+    fn: function(req, res) {
+      res.jsonp(200, {
+        message: 'Request received successfully!',
+        method: req.method
+      });
+    }
+  },
+  delete: {
+    method: 'DELETE',
+    fn: function(req, res) {
+      res.jsonp(200, {
+        message: 'Request received successfully!',
+        method: req.method
+      });
+    }
   }
- },
- post: {
-  method: 'POST',
-  fn: function (req, res) {
-   res.jsonp(200, {
-    message: 'Request received successfully!',
-    method: req.method
-   });
-  }
- },
- put: {
-  method: 'PUT',
-  fn: function (req, res) {
-   res.jsonp(200, {
-    message: 'Request received successfully!',
-    method: req.method
-   });
-  }
- },
- delete: {
-  method: 'DELETE',
-  fn: function (req, res) {
-   res.jsonp(200, {
-    message: 'Request received successfully!',
-    method: req.method
-   });
-  }
- }
 };
